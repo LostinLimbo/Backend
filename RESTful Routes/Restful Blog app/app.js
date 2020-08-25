3f8118c578fbd6483ea64072efeeb78fdeb77435
Ben Mortimer <50639398+LostinLimbo@users.noreply.github.com>
2020-08-25 22:12:05 +0100

    creating blog app
    created headers and footers, more on the index page (showing blog)

--- RESTful Routes/Restful Blog app/app.js
+++ RESTful Routes/Restful Blog app/app.js
@@ -3,22 +3,35 @@ var bodyParser = require("body-parser"),
     express = require("express"),
     app = express();
 
+// APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
+// MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
   title: String,
   image: String,
   body: String,
   created: {type: Date, default: Date.now}
 });
-// title
-// image
-// body
-// created
+var Blog = mongoose.model("Blog", blogSchema);
+
+// RESTFUL ROUTES
+app.get("/", (req, res) =>{
+  res.redirect("/blogs");
+});
+app.get("/blogs", (req, res) =>{
+  Blog.find({}, function(err, blogs){
+    if(err){
+      console.log("ERROR");
+    } else{
+      res.render("index", {blogs: blogs});
+    }
+  })
+});
 
 app.listen(process.env.PORT || 80, process.env.IP, function(){
   console.log("SERVER IS RUNNING");
