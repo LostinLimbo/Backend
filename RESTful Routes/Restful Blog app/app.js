78b55469f76a8dd1458f2ae153770041926732ae
Ben Mortimer <50639398+LostinLimbo@users.noreply.github.com>
2020-09-09 09:47:40 +0100

    ROUTE UPDATES

--- RESTful Routes/Restful Blog app/app.js
+++ RESTful Routes/Restful Blog app/app.js
@@ -1,7 +1,8 @@
 var bodyParser = require("body-parser"),
-    mongoose = require("mongoose"),
-    express = require("express"),
-    app = express();
+methodOverride = require("method-override"),
+mongoose       = require("mongoose"),
+express        = require("express"),
+app             = express();
 
 // APP CONFIG
 mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true, useUnifiedTopology: true });
@@ -9,7 +10,7 @@ mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(methodOverride("_method"));
 // MONGOOSE/MODEL CONFIG
 var blogSchema = new mongoose.Schema({
   title: String,
@@ -51,8 +52,47 @@ app.post("/blogs", (req, res) =>{
 });
 //  SHOW ROUTE
 app.get("/blogs/:id", (req, res) =>{
-  res.render("show");
+  Blog.findById(req.params.id, (err, foundBlog) =>{
+    if(err){
+      res.redirect("/blogs");
+    } else{
+        res.render("show", {blog: foundBlog});
+    }
+  });
 });
+// EDIT ROUTE
+app.get("/blogs/:id/edit", (req, res) =>{
+  Blog.findById(req.params.id, (err, foundBlog) =>{
+    if(err){
+      res.redirect("/blogs");
+    } else{
+      res.render("edit", {blog: foundBlog});
+    }
+  });
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", (req, res) =>{
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, (err, updatedBlog) =>{
+    if(err){
+      res.redirect("/blogs");
+    } else{
+      res.redirect("/blogs/" + req.params.id);
+    }
+  });
+});
+
+// DELETE ROUTE
+app.delete("/blogs/:id", (req, res) =>{
+  Blog.findByIdAndDelete(req.params.id, (err, deletedBlog) =>{
+    if(err){
+      res.redirect("/blogs/" + req.params.id);
+    } else{
+       res.redirect("/blogs");
+    }
+  });
+});
+
 
 app.listen(process.env.PORT || 80, process.env.IP, function(){
   console.log("SERVER IS RUNNING");
