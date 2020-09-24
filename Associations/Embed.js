a0d130b96371f767ae49c2d85f5357609dd8e3d5
Ben Mortimer <50639398+LostinLimbo@users.noreply.github.com>
2020-09-24 16:11:07 +0100

    Update Embed.js
    Association tutorial

--- Associations/Embed.js
+++ Associations/Embed.js
@@ -1,24 +1,67 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo", { useNewUrlParser: true, useUnifiedTopology: true });
 
-// USER - email, name
-var userSchema = new mongoose.Schema({
-    email: String,
-    name: String
-});
-
-var User = mongoose.model("User", userSchema);
-
 // POST - title, content
 var postSchema = new mongoose.Schema({
     title: String,
     content: String
 });
+var Post = mongoose.model("Post", postSchema);
 
-var postModel = mongoose.model("Post", postSchema);
+// USER - email, name
+var userSchema = new mongoose.Schema({
+    email: String,
+    name: String,
+    posts: [postSchema]
+});
+var User = mongoose.model("User", userSchema);
+
+// var newUser = new User({
+//     email: "ben.mort@me.com",
+//     name: "Ben Mortimer"
+// });
 
 
-var newUser = new User({
-    email: "charlie@brown.edu",
-    name: "Charlie Brown"
+// newUser.posts.push({
+//     title: "How to play",
+//     content: "Just do it"
+// });
+
+// newUser.save(function(err, user){
+//     if(err){
+//         console.log(err);
+//     }   else{
+//         console.log(user);
+//     }
+// });
+
+// var newPost = new Post({
+//     title: "Reflections on Apples",
+//     content: "They are Delicious"
+// });
+
+// newPost.save((err, post) =>{
+//     if(err){
+//         console.log(err);
+//     }   else{
+//         console.log(post);
+//     }
+// });
+
+User.findOne({name: "Ben Mortimer"}, (err, user) =>{
+    if(err){
+        // console.log(err);
+    }   else{
+        user.posts.push({
+            title: "Things I HATE!",
+            content: "Wet feet"
+        });
+        user.save((err, user) =>{
+            if(err){
+                console.log(err);
+            }   else{
+                console.log(user);
+            }
+        });
+    }
 });
