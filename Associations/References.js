21122cc7621a0543c50bbc18f8b13d38b0d98126
Ben Mortimer <50639398+LostinLimbo@users.noreply.github.com>
2020-09-24 17:23:36 +0100

    Update
    Tidied up code by creating models

--- Associations/References.js
+++ Associations/References.js
@@ -1,56 +1,40 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/blog_demo_2", { useNewUrlParser: true, useUnifiedTopology: true });
 
-// POST - title, content
-var postSchema = new mongoose.Schema({
-    title: String,
-    content: String
-});
-var Post = mongoose.model("Post", postSchema);
+var Post = require("./models/post");
+var User = require("./models/user");
 
-// USER - email, name
-var userSchema = new mongoose.Schema({
-    email: String,
-    name: String,
-    posts: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Post"
-      }
-    ]
-});
-var User = mongoose.model("User", userSchema);
 
 // Find user
 // Find all posts for that user
 
-User.findOne({email: "bill@feet.com"}).populate("posts").exec((err, user) =>{
-  if(err){
-    console.log(err);
-  } else{
-    console.log(user);
-  }
-});
-
-// Post.create({
-//   title: "How to properly burn decent burgers",
-//   content: "Cook, Cook, Cook Cook, Cook, Cook, Cook, Cook"
-// }, (err, post) =>{
-//   User.findOne({email: "bill@feet.com"}, (err, foundUser) =>{
-//     if(err){
-//       console.log(err);
-//     } else{
-//       foundUser.posts.push(post);
-//       foundUser.save((err, data) =>{
-//         if(err){
-//           console.log(err)
-//         } else{
-//           console.log(data);
-//         }
-//       });
-//     }
-//   });
+// User.findOne({email: "bill@feet.com"}).populate("posts").exec((err, user) =>{
+//   if(err){
+//     console.log(err);
+//   } else{
+//     console.log(user);
+//   }
 // });
+
+Post.create({
+  title: "Things to look at",
+  content: "Peacocks, Yourself, A Mirror"
+}, (err, post) =>{
+  User.findOne({email: "bill@feet.com"}, (err, foundUser) =>{
+    if(err){
+      console.log(err);
+    } else{
+      foundUser.posts.push(post);
+      foundUser.save((err, data) =>{
+        if(err){
+          console.log(err)
+        } else{
+          console.log(data);
+        }
+      });
+    }
+  });
+});
 
 // User.create({
 //   email: "bill@feet.com",
